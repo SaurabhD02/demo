9b07b488965d6758ee1f150ba47696e365e45e39
SaurabhD02 <saurabhdorle02@gmail.com>
2025-02-26 15:29:07 +0530

    Update DemoApplication.java

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -11,7 +11,7 @@ public class DemoApplication {
 
 	@GetMapping("/")
 	public String message(){
-		return "Dockerizing Spring boot application with Google Jib";
+		return "Dockerizing Spring boot application with Google Jib using Jenkins";
 	}
 
 	public static void main(String[] args) {
